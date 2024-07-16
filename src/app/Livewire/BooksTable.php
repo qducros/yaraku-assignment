a959bb9584fec02453697ea253bb1c39897b8966
q.ducros <q.ducros@gmail.com>
2024-07-16 10:24:02 +0900

    Create html table and send all the books

--- src/app/Livewire/BooksTable.php
+++ src/app/Livewire/BooksTable.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire;
 
+use App\Models\Book;
 use Livewire\Component;
 
 class BooksTable extends Component
 {
     public function render()
     {
-        return view('livewire.books-table');
+        return view('livewire.books-table', [
+            'books' => Book::all(),
+        ]);
     }
 }
--- src/resources/views/livewire/books-table.blade.php
+++ src/resources/views/livewire/books-table.blade.php
@@ -1,3 +1,40 @@
-<div class="content">
-    <h1>Welcome</h1>
+<div>
+    <table class="table is-fullwidth">
+        <thead class="has-background-info">
+            <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Last modified</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($books as $book)
+                <div wire:key="{{ $book->id }}">
+                    <tr>
+                        <td>
+                            <span class="has-text-black has-text-weight-bold">
+                                {{ $book->title }}
+                            </span>
+                        </td>
+                        <td>
+                            {{ $book->author }}
+                        </td>
+                        <td>
+                            {{ $book->updated_at }}
+                        </td>
+                        <td>
+                            Actions
+                        </td>
+                    </tr>
+                </div>
+            @empty
+                <tr>
+                    <td colspan="4" class="has-background-light">
+                        No books found.
+                    </td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
 </div>
