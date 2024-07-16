cbfa1c407113c538e3fd3b8fbb8b085ac850ec53
q.ducros <q.ducros@gmail.com>
2024-07-16 11:52:10 +0900

    Create tests for table sorting

--- src/tests/Feature/BooksTableTest.php
+++ src/tests/Feature/BooksTableTest.php
@@ -26,9 +26,79 @@ class BooksTableTest extends TestCase
 
     public function test_component_contains_non_empty_table()
     {
-        Book::factory(1)->create(['title' => 'LoTR']);
+        Book::factory()->create(['title' => 'LoTR']);
 
         Livewire::test(BooksTable::class)
             ->assertSee('LoTR');
+    }
+
+    public function test_sort_by_title_asc_by_default()
+    {
+        Book::factory()->create(['title' => 'LoTR']);
+        Book::factory()->create(['title' => 'Harry Potter']);
+ 
+        Livewire::test(BooksTable::class)
+            ->assertSeeInOrder(['Harry Potter', 'LoTR']);
+    }
+
+    public function test_can_sort_books_by_title_via_table_header_click()
+    {
+        Book::factory()->create(['title' => 'LoTR']);
+        Book::factory()->create(['title' => 'Harry Potter']);
+        
+        // Since title/ASC is the default order, call setOrderField('title') will set title/DESC order
+        Livewire::test(BooksTable::class)
+            ->call('setOrderField', 'title')
+            ->assertSeeInOrder(['LoTR', 'Harry Potter']);
+    }
+
+    public function test_can_sort_books_by_title_via_url_query_string()
+    {
+        Book::factory()->create(['title' => 'LoTR']);
+        Book::factory()->create(['title' => 'Harry Potter']);
+ 
+        Livewire::withQueryParams(['orderField' => 'title', 'orderDirection' => 'ASC'])
+            ->test(BooksTable::class)
+            ->assertSeeInOrder(['Harry Potter', 'LoTR']);
+    }
+
+    public function test_can_sort_books_by_author_via_table_header_click()
+    {
+        Book::factory()->create(['author' => 'Tolkien']);
+        Book::factory()->create(['author' => 'Gemmel']);
+        
+        Livewire::test(BooksTable::class)
+            ->call('setOrderField', 'author')
+            ->assertSeeInOrder(['Gemmel', 'Tolkien']);
+    }
+
+    public function test_can_sort_books_by_author_via_url_query_string()
+    {
+        Book::factory()->create(['author' => 'Tolkien']);
+        Book::factory()->create(['author' => 'Gemmel']);
+ 
+        Livewire::withQueryParams(['orderField' => 'author', 'orderDirection' => 'ASC'])
+            ->test(BooksTable::class)
+            ->assertSeeInOrder(['Gemmel', 'Tolkien']);
+    }
+
+    public function test_can_sort_books_by_last_updated_via_table_header_click()
+    {
+        Book::factory()->create(['author' => 'Tolkien']);
+        Book::factory()->create(['author' => 'Gemmel']);
+        
+        Livewire::test(BooksTable::class)
+            ->call('setOrderField', 'updated_at')
+            ->assertSeeInOrder(['Tolkien', 'Gemmel']);
+    }
+
+    public function test_can_sort_books_by_last_updated_via_url_query_string()
+    {
+        Book::factory()->create(['author' => 'Tolkien']);
+        Book::factory()->create(['author' => 'Gemmel']);
+ 
+        Livewire::withQueryParams(['orderField' => 'updated_at', 'orderDirection' => 'ASC'])
+            ->test(BooksTable::class)
+            ->assertSeeInOrder(['Tolkien', 'Gemmel']);
     }
 }
