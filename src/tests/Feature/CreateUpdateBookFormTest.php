642701ee056a5cafb782806c594fce0a070a272f
q.ducros <q.ducros@gmail.com>
2024-07-16 19:52:34 +0900

    Create tests for book edit

--- src/tests/Feature/CreateUpdateBookFormTest.php
+++ src/tests/Feature/CreateUpdateBookFormTest.php
@@ -13,7 +13,8 @@ class CreateUpdateBookFormTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_component_exists_on_the_page()
+    // Test on book creation
+    public function test_create_component_exists_on_the_page()
     {
         Livewire::test(BooksTable::class)
             ->call('setAction', 'create')
@@ -86,5 +87,126 @@ class CreateUpdateBookFormTest extends TestCase
             ->assertSet('action', 'create')
             ->call('setAction', 'create')
             ->assertSet('action', '');
+    }
+
+    // Test on book edit
+    public function test_edit_component_exists_on_the_page()
+    {
+        $book = Book::factory()->create();
+
+        Livewire::test(BooksTable::class)
+            ->call('setAction', 'edit-'.$book->id)
+            ->assertSet('action', 'edit-'.$book->id)
+            ->assertSeeLivewire(CreateUpdateBookForm::class);
+    }
+
+    public function test_edit_component_displays_current_book_title_and_author()
+    {
+        $book = Book::factory()->create(['title' => 'Lord of the Rings', 'author' => 'Tolkien']);
+
+        Livewire::test(BooksTable::class)
+            ->call('setAction', 'edit-'.$book->id)
+            ->assertSet('action', 'edit-'.$book->id);
+        Livewire::test(CreateUpdateBookForm::class, ['book' => $book, 'action' => 'edit-'.$book->id])
+            ->assertSeeHtml('Lord of the Rings')
+            ->assertSeeHtml('Tolkien')
+            ->assertSet('form.title', 'Lord of the Rings')
+            ->assertSet('form.author', 'Tolkien');
+    }
+
+    public function test_can_edit_book_with_valid_input()
+    {
+        $book = Book::factory()->create(['title' => 'Lord of the Rings', 'author' => 'Tolkien']);
+
+        Livewire::test(BooksTable::class)
+            ->call('setAction', 'edit-'.$book->id)
+            ->assertSet('action', 'edit-'.$book->id);
+        Livewire::test(CreateUpdateBookForm::class, ['book' => $book, 'action' => 'edit-'.$book->id])
+            ->set(['form.title' => 'Waylander', 'form.author'=> 'Gemmell'])
+            ->call('save')
+            ->assertDispatched('createUpdateBook');
+        Livewire::test(BooksTable::class)
+            ->assertSet('action', '')
+            ->assertSeeInOrder(['Waylander', 'Gemmell']);
+    }
+
+    public function test_cant_edit_book_with_too_short_input()
+    {
+        $book = Book::factory()->create(['title' => 'Lord of the Rings', 'author' => 'Tolkien']);
+
+        Livewire::test(BooksTable::class)
+            ->call('setAction', 'edit-'.$book->id)
+            ->assertSet('action', 'edit-'.$book->id);
+        Livewire::test(CreateUpdateBookForm::class, ['book' => $book, 'action' => 'edit-'.$book->id])
+            ->set(['form.title' => 'book', 'form.author'=> 'auth'])
+            ->call('save')
+            ->assertHasErrors(['form.title' => ['min:5']])
+            ->assertHasErrors(['form.author' => ['min:5']])
+            ->assertNotDispatched('createUpdateBook')
+            ->assertSee('The title field must be at least 5 characters.')
+            ->assertSee('The author field must be at least 5 characters.');
+    }
+
+    public function test_cant_edit_book_without_required_input()
+    {
+        $book = Book::factory()->create(['title' => 'Lord of the Rings', 'author' => 'Tolkien']);
+
+        Livewire::test(BooksTable::class)
+            ->call('setAction', 'edit-'.$book->id)
+            ->assertSet('action', 'edit-'.$book->id);
+        Livewire::test(CreateUpdateBookForm::class, ['book' => $book, 'action' => 'edit-'.$book->id])
+            ->set(['form.title' => '', 'form.author'=> ''])
+            ->call('save')
+            ->assertHasErrors(['form.title' => ['required']])
+            ->assertHasErrors(['form.author' => ['required']])
+            ->assertNotDispatched('createUpdateBook')
+            ->assertSee('The title field is required.')
+            ->assertSee('The author field is required.');
+    }
+
+    public function test_can_cancel_book_edit()
+    {
+        $book = Book::factory()->create(['title' => 'Lord of the Rings', 'author' => 'Tolkien']);
+
+        Livewire::test(BooksTable::class)
+            ->call('setAction', 'edit-'.$book->id)
+            ->assertSet('action', 'edit-'.$book->id);
+        Livewire::test(CreateUpdateBookForm::class, ['book' => $book, 'action' => 'edit-'.$book->id])
+            ->set(['form.title' => 'This is a book ', 'form.author' => 'This is an author'])
+            ->call('cancel')
+            ->assertDispatched('cancelAction');
+        Livewire::test(BooksTable::class)
+            ->assertSet('action', '')
+            ->assertSee(['Lord of the Rings', 'Tolkien']);
+    }
+
+    public function test_can_cancel_book_edit_by_reclicking_edit()
+    {
+        $book = Book::factory()->create(['title' => 'Lord of the Rings', 'author' => 'Tolkien']);
+
+        Livewire::test(BooksTable::class)
+            ->call('setAction', 'edit-'.$book->id)
+            ->assertSet('action', 'edit-'.$book->id)
+            ->call('setAction', 'edit-'.$book->id)
+            ->assertSet('action', '')
+            ->assertSee(['Lord of the Rings', 'Tolkien']);
+    }
+
+    public function test_can_cancel_book_edit_by_clicking_another_edit()
+    {
+        $book1 = Book::factory()->create(['title' => 'Lord of the Rings', 'author' => 'Tolkien']);
+        $book2 = Book::factory()->create(['title' => 'Silmarillion', 'author' => 'Tolkien']);
+
+        Livewire::test(BooksTable::class)
+            ->call('setAction', 'edit-'.$book1->id)
+            ->assertSet('action', 'edit-'.$book1->id)
+            ->call('setAction', 'edit-'.$book2->id)
+            ->assertSet('action', 'edit-'.$book2->id)
+            ->assertSee(['Lord of the Rings', 'Tolkien', 'Silmarillion']);
+        Livewire::test(CreateUpdateBookForm::class, ['book' => $book2, 'action' => 'edit-'.$book2->id])
+            ->assertSeeHtml('Silmarillion')
+            ->assertSeeHtml('Tolkien')
+            ->assertSet('form.title', 'Silmarillion')
+            ->assertSet('form.author', 'Tolkien');
     }
 }
