247eed3d03f240e548eb66601c9cb112b6233670
q.ducros <q.ducros@gmail.com>
2024-07-16 09:53:29 +0900

    Create livewire table component and layout

--- src/resources/views/components/layouts/app.blade.php
+++ src/resources/views/components/layouts/app.blade.php
@@ -2,21 +2,18 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css'])
 
+        <title>{{ $title ?? 'Yaraku assignment' }}</title>
     </head>
     <body>
-        <div class="content">
-            <h1>Welcome</h1>
-        </div>
+        {{ $slot }}
     </body>
 </html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire\BooksTable;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('books', BooksTable::class)->name('books');
